4e9b66850c9d5c8638c23e61be03b3c27d75a64e
im2sh <104557685+im2sh@users.noreply.github.com>
2022-08-24 20:21:12 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 130372 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+card = deque()
+
+for i in range(1,n+1):
+    card.append(i)
+
+while len(card) != 1:
+    card.popleft()
+    temp = card.popleft()
+    card.append(temp)
+
+print(card[0])
