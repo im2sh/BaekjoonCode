75ec65a7d207d7b89f03e1e57b075cede36f6a8f
im2sh <104557685+im2sh@users.noreply.github.com>
2023-06-06 01:20:07 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s[5];
+int main() {	
+	for(int i=0;i<5;i++)
+		cin>>s[i];
+	for(int i=0;i<15;i++){
+		for(int j=0;j<5;j++){
+			if(s[j].length()>i)
+				cout<<s[j][i];		
+		}
+	}
+	return 0;
+}
