353d00825cad4180fc21100bb3bd270465266e68
im2sh <104557685+im2sh@users.noreply.github.com>
2023-06-26 22:51:02 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(nullptr);
+    cout.tie(nullptr);
+	ll N; 
+    cin >> N;
+	cout << N * N << '\n' << 2;
+    return 0;
+}
