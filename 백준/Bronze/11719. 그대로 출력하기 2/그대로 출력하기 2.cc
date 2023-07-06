ccde981a1cd1359b465f9390ad718d2900cef073
im2sh <104557685+im2sh@users.noreply.github.com>
2023-07-06 15:04:46 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string s; 
+    int i;
+    while(i < 100)
+    {
+        getline(cin, s);
+
+        cout << s << "\n";
+        
+        i++;
+    }
+
+    return 0;
+}
