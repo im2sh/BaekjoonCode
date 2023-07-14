a7959c600660722bfec2624396d92e0d28b4672d
im2sh <104557685+im2sh@users.noreply.github.com>
2023-07-14 20:16:13 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+typedef long long ll;
+
+ll N, temp;
+stack<pair<ll, ll>> st;
+ll ret = 0;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N;
+}
+
+void solve() {
+    for(int i = 0; i < N; i++){
+        cin >> temp;
+        int cnt = 1;
+        while(st.size() && st.top().first <= temp){
+            ret += st.top().second;
+            if(st.top().first == temp){
+                cnt = st.top().second + 1;
+            }else{
+                cnt = 1;
+            }
+            st.pop();
+        }
+        if(st.size()) ret++;
+        st.push({temp, cnt});
+    }
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    cout << ret << "\n";
+    return 0;
+}
