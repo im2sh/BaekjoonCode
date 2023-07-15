b3ac3888f83a46482e8bda61fcc2aa8994ec5a06
im2sh <104557685+im2sh@users.noreply.github.com>
2023-07-15 20:46:39 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string a, b;
+	cin >> a >> b;
+    if(a.size() < b.size())
+        cout << "no" << "\n";
+    else
+        cout << "go" << "\n";
+    return 0;
+}
