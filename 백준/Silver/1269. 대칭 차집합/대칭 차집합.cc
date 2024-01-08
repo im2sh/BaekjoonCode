3c630fe4508c45db7f88d59f035d636059416980
im2sh <104557685+im2sh@users.noreply.github.com>
2024-01-08 16:53:00 +0900

    [Silver IV] Title: 대칭 차집합, Time: 236 ms, Memory: 20768 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -1,20 +1,36 @@
 #include <iostream>
 #include <map>
+
 using namespace std;
-map<int, bool> m;
-int N,M;
-int num;
-int main(void)
-{
-	cin >> N >> M;
-	for (int i = 0; i < N+M; i++)
-	{
-		cin >> num;
-		if (m[num] == true)
-			m.erase(num);
-		else 
-			m[num] = true;
-	}
-	cout << m.size();
+
+int N, M;
+map<int, bool> MP;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N >> M;
+}
+
+void solve() {
+    int temp;
+    for (int i = 0; i < N + M; i++) {
+        cin >> temp;
+        if (MP[temp])
+            MP.erase(temp);
+        else
+            MP[temp] = true;
+    }
+    cout << MP.size();
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
     return 0;
 }
