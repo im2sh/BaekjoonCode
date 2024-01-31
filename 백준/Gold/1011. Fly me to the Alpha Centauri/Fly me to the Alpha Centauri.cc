71e40305ab5c23d50de4ccf168ed992c07f63453
im2sh <104557685+im2sh@users.noreply.github.com>
2024-01-31 19:52:27 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int TC, x, y;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> TC;
+}
+
+void solve() {
+    while (TC--) {
+        cin >> x >> y;
+        int dist = y - x;
+        if (dist <= 3) {
+            cout << dist << '\n';
+            continue;
+        }
+
+        double midN = double(sqrt(dist));
+        int basic = floor(midN);
+        
+        if (basic == midN)
+            cout << (midN * 2) - 1 << '\n';
+        else if (basic + 0.5 > midN)
+            cout << (basic * 2) << '\n';
+        else if (basic + 0.5 < midN)
+            cout << (basic * 2) + 1 << '\n';
+    }
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
