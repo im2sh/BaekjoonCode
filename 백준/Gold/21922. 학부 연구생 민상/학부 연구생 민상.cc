d1f4d3b7f5076018a28499509e3b289532296583
im2sh <104557685+im2sh@users.noreply.github.com>
2024-03-22 19:54:20 +0900

    [Gold V] Title: 학부 연구생 민상, Time: 504 ms, Memory: 33304 KB -BaekjoonHub

--- 백준/Gold/21922. 학부 연구생 민상/학부 연구생 민상.cc
+++ 백준/Gold/21922. 학부 연구생 민상/학부 연구생 민상.cc
@@ -5,11 +5,10 @@
 using namespace std;
 const int dy[4] = {-1, 1, 0, 0};
 const int dx[4] = {0, 0, -1, 1};
-int N, M;
+int N, M, ret;
 int BOARD[2001][2001];
-int visisted[2001][2001][4];
 vector<pair<int, int>> fan;
-int ret[2001][2001];
+int visited[2001][2001];
 
 void FastIO() {
     ios_base::sync_with_stdio(false);
@@ -30,10 +29,10 @@ void Init() {
 
 void go(int y, int x) {
     queue<pair<pair<int, int>, int>> q;
-    for (int i = 0; i < 4; i++) {
-        q.push({{y, x}, i});
-        visisted[y][x][i] = true;
-    }
+    q.push({{y, x}, 0});
+    q.push({{y, x}, 1});
+    q.push({{y, x}, 2});
+    q.push({{y, x}, 3});
 
     while (q.size()) {
         int cy = q.front().first.first;
@@ -44,12 +43,11 @@ void go(int y, int x) {
         int ny = cy + dy[dir];
         int nx = cx + dx[dir];
 
-        if (ny < 0 || ny >= N || nx < 0 || nx >= M || visisted[ny][nx][dir])
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M)
             continue;
-
-        ret[ny][nx] = 1;
-        visisted[ny][nx][dir] = 1;
-
+        if (BOARD[ny][nx] == 9)
+            continue;
+        visited[ny][nx] = 1;
         if (BOARD[ny][nx] == 1) {
             if (dir == 2 || dir == 3)
                 continue;
@@ -79,21 +77,24 @@ void go(int y, int x) {
     }
 }
 
+void bfs(int y, int x) {
+    queue<pair<int, int>> q;
+    go(y, x);
+}
+
 void solve() {
     for (int i = 0; i < fan.size(); i++) {
-        int y = fan[i].first;
-        int x = fan[i].second;
-        ret[y][x] = 1;
-        go(y, x);
+        bfs(fan[i].first, fan[i].second);
+        visited[fan[i].first][fan[i].second] = 1;
     }
-    int sum = 0;
 
     for (int y = 0; y < N; y++) {
         for (int x = 0; x < M; x++) {
-            sum += ret[y][x];
+            if (visited[y][x])
+                ret++;
         }
     }
-    cout << sum;
+    cout << ret;
 }
 
 int main(void) {
