538260093110cd390eb1989fa3374adb0f6e904f
im2sh <104557685+im2sh@users.noreply.github.com>
2024-04-08 20:58:35 +0900

    [Gold IV] Title: 킥보드로 등교하기, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/27977. 킥보드로 등교하기/킥보드로 등교하기.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int L, N, K, ret = 987654321;
+vector<int> charger;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> L >> N >> K;
+    charger.push_back(0);
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        charger.push_back(temp);
+    }
+    charger.push_back(L);
+}
+
+bool check(int mid) {
+    int gas = mid;
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        int dist = charger[i] - charger[i - 1];
+        gas -= dist;
+        if (gas < 0) {
+            return false;
+        }
+        if (gas < (charger[i + 1] - charger[i])) {
+            gas = mid;
+            cnt++;
+        }
+    }
+
+    if (cnt > K || (gas + charger[N]) < L)
+        return false;
+    return true;
+}
+
+void solve() {
+    int diff = 0;
+    int low = 0;
+    int high = L;
+    int mid = 0;
+    for (int i = 1; i <= N + 1; i++) {
+        diff = max(diff, charger[i] - charger[i - 1]);
+    }
+    low = diff;
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (check(mid)) {
+            ret = min(ret, mid);
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    cout << ret;
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
