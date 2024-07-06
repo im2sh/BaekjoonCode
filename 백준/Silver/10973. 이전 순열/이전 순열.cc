922c411b673fc7218994af2f8320ddc1d9dc05ae
im2sh <104557685+im2sh@users.noreply.github.com>
2024-07-06 23:09:05 +0900

    [Silver III] Title: 이전 순열, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int N;
+vector<int> arr;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
+}
+
+void solve() {
+    if (prev_permutation(arr.begin(), arr.end())) {
+        for (int i = 0; i < N; i++) {
+            cout << arr[i] << " ";
+        }
+    } else {
+        cout << "-1";
+    }
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
