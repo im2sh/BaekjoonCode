efeb0f4537728a1d7a171e689359996a123ff417
im2sh <104557685+im2sh@users.noreply.github.com>
2024-08-16 16:57:50 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 126 ms, Memory: 19,424 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+    static int TC;
+    static BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int[] password = new int[9];
+    static Queue<Integer> q = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 1; i <= 10; i++) {
+            Init();
+            solve();
+        }
+        System.out.print(sb);
+    }
+
+    public static void Init() throws IOException {
+        TC = Integer.parseInt(br.readLine());
+        q.clear();
+        for (int i = 0; i < 9; i++) {
+            password[i] = 0;
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 8; i++) {
+            password[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+
+    public static boolean check(){
+        boolean flag = false;
+        boolean zeroFlag = false;
+        for(int i = 0; i < 8; i++){
+            Integer cur = q.poll();
+            q.offer(cur);
+            if(cur >= 10)
+                flag = true;
+            if(cur <= 0)
+                zeroFlag = true;
+        }
+        if(!zeroFlag)
+            return true;
+        return false;
+    }
+
+    public static void solve() {
+        for (int i = 0; i < 8; i++) {
+            password[i] %= 120;
+            q.offer(password[i]);
+        }
+
+        int minusNum = 1;
+        while (check()) {
+            int cur = q.poll();
+            cur -= minusNum;
+            if (cur <= 0) {
+                q.offer(0);
+            }else{
+                q.offer(cur);
+            }
+            minusNum = (minusNum % 5) + 1;
+        }
+        sb.append("#").append(TC).append(" ");
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            sb.append(cur).append(" ");
+        }
+        sb.append("\n");
+    }
+}
