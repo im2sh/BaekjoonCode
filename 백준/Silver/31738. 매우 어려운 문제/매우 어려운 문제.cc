fc8538f12869897f3202268b0f03b581e6b6b5c9
im2sh <104557685+im2sh@users.noreply.github.com>
2024-09-30 23:39:21 +0900

    [Silver V] Title: 매우 어려운 문제, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+ll N, M, ret = 1;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N >> M;
+}
+
+void solve() {
+    if (N > M)
+        cout << 0;
+    else {
+        if (N == 1)
+            cout << 1;
+        else {
+            for (long long i = 2; i <= N; i++)
+                ret = (ret * i) % M;
+            cout << ret;
+        }
+    }
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
