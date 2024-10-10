9adf7bbe3df0eb71ea478be61fdd9b4e445e6e6c
im2sh <104557685+im2sh@users.noreply.github.com>
2024-10-10 16:03:08 +0900

    [Gold I] Title: 낚시왕, Time: 160 ms, Memory: 2444 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -1,18 +1,20 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 using namespace std;
 
 typedef struct shark {
-    int y, x, s, d, z;
-} Shark;
+    char name;
+    int s;
+    int d;
+    int z;
+    bool alive;
+} SHARK;
 
-typedef struct board {
-    vector<Shark> fish;
-} BOARD;
-
+SHARK shark[104][104];
+SHARK temp_shark[104][104];
+pair<int, int> target;
 int R, C, M, ret;
-BOARD B[104][104];
 const int dy[4] = {-1, 1, 0, 0};
 const int dx[4] = {0, 0, 1, -1};
 
@@ -23,120 +25,132 @@ void FastIO() {
 }
 
 void Init() {
+    char alpha = 'a';
     int a, b, c, d, e;
     cin >> R >> C >> M;
     for (int i = 0; i < M; i++) {
         cin >> a >> b >> c >> d >> e;
-        Shark temp;
-        temp.y = a - 1;
-        temp.x = b - 1;
-        if ((d - 1) <= 1)
-            temp.s = c % ((R - 1) * 2);
+        shark[a][b].name = alpha;
+        alpha++;
+        if (d - 1 <= 1)
+            c %= (2 * (R - 1));
         else
-            temp.s = c % ((C - 1) * 2);
-        temp.d = d - 1;
-        temp.z = e;
-        B[a - 1][b - 1].fish.push_back(temp);
+            c %= (2 * (C - 1));
+        shark[a][b].s = c;
+        shark[a][b].d = d - 1;
+        shark[a][b].z = e;
+        shark[a][b].alive = true;
     }
 }
 
-void findAndCatch(int c) {
-    for (int y = 0; y < R; y++) {
-        if (B[y][c].fish.size()) {
-            ret += B[y][c].fish[0].z;
-            B[y][c].fish.clear();
+void findAndCatchTarget(int x) {
+    target = {-1, -1};
+    for (int y = 1; y <= R; y++) {
+        if (shark[y][x].alive) {
+            target = {y, x};
             break;
         }
     }
+    if (shark[target.first][target.second].alive) {
+        ret += shark[target.first][target.second].z;
+        shark[target.first][target.second].alive = false;
+    }
 }
 
+int changeDir(int d) {
+    switch (d) {
+        case 0:
+            return 1;
+        case 1:
+            return 0;
+        case 2:
+            return 3;
+        case 3:
+            return 2;
+    }
+}
+
+void move(int y, int x) {
+    int cy = y;
+    int cx = x;
+    shark[y][x].alive = false;
+    SHARK temp = shark[y][x];
+    while (temp.s) {
+        int ny = cy + dy[temp.d];
+        int nx = cx + dx[temp.d];
+
+        if (ny == 0 || ny == R + 1) {
+            ny = cy;
+            temp.s++;
+            temp.d = changeDir(temp.d);
+        } else if (nx == 0 || nx == C + 1) {
+            nx = cx;
+            temp.s++;
+            temp.d = changeDir(temp.d);
+        }
+        temp.s--;
+        cy = ny;
+        cx = nx;
+    }
+
+    if (temp.z > temp_shark[cy][cx].z) {
+        temp_shark[cy][cx].name = temp.name;
+        temp_shark[cy][cx].d = temp.d;
+        temp_shark[cy][cx].s = shark[y][x].s;
+        temp_shark[cy][cx].z = shark[y][x].z;
+        temp_shark[cy][cx].alive = true;
+    }
+}
 
 void display() {
-    for (int y = 0; y < R; y++) {
-        for (int x = 0; x < C; x++) {
-            if (B[y][x].fish.size()) {
-                cout << B[y][x].fish[0].z << " ";
-            } else {
-                cout << 0 << " ";
-            }
+    for (int y = 1; y <= R; y++) {
+        for (int x = 1; x <= C; x++) {
+            if (shark[y][x].alive)
+                cout << shark[y][x].name << " ";
+            else
+                cout << "x" << " ";
         }
         cout << "\n";
     }
 }
 
-void SharkMove() {
-    vector<Shark> ts;
-    int ny = 0;
-    int nx = 0;
-    Shark temp;
-    BOARD temp_board[104][104];
-    for (int y = 0; y < R; y++) {
-        for (int x = 0; x < C; x++) {
-            if (B[y][x].fish.size())
-                ts.push_back(B[y][x].fish[0]);
+void displayDir() {
+    for (int y = 1; y <= R; y++) {
+        for (int x = 1; x <= C; x++) {
+            if (shark[y][x].alive)
+                cout << shark[y][x].alive << " ";
+            else
+                cout << "x" << " ";
         }
-    }
-    for (int i = 0; i < ts.size(); i++) {
-        int cy = ts[i].y;
-        int cx = ts[i].x;
-        int cs = ts[i].s;
-        int cd = ts[i].d;
-        int cz = ts[i].z;
-
-        ny = cy;
-        nx = cx;
-        for (int j = 0; j < cs; j++) {
-            ny = ny + dy[cd];
-            nx = nx + dx[cd];
-
-            if (cd <= 1) {
-                if (ny == -1) {
-                    cd = 1;
-                    ny = 1;
-                } else if (ny == R) {
-                    cd = 0;
-                    ny = R - 2;
-                }
-            } else {
-                if (nx == -1) {
-                    cd = 2;
-                    nx = 1;
-                } else if (nx == C) {
-                    cd = 3;
-                    nx = C - 2;
-                }
-            }
-        }
-        temp = {ny, nx, cs, cd, cz};
-        if (!temp_board[ny][nx].fish.size()) {
-            temp_board[ny][nx].fish.push_back(temp);
-        } else {
-            if (temp_board[ny][nx].fish[0].z > temp.z)
-                continue;
-            else {
-                temp_board[ny][nx].fish.pop_back();
-                temp_board[ny][nx].fish.push_back(temp);
-            }
-        }
-    }
-    for (int y = 0; y < R; y++) {
-        for (int x = 0; x < C; x++) {
-            B[y][x] = temp_board[y][x];
-        }
+        cout << "\n";
     }
 }
 
+
 void solve() {
-    for (int i = 0; i <= C; i++) {
-        findAndCatch(i);
-        SharkMove();
+    SHARK temp;
+    temp.name = 'x';
+    temp.d = 0;
+    temp.z = 0;
+    temp.s = 0;
+    temp.alive = false;
+    for (int i = 1; i <= C; i++) {
+        fill(&temp_shark[0][0], &temp_shark[0][0] + 104 * 104, temp);
+        findAndCatchTarget(i);
+        for (int y = 1; y <= R; y++) {
+            for (int x = 1; x <= C; x++) {
+                if (shark[y][x].alive)
+                    move(y, x);
+            }
+        }
+        memcpy(shark, temp_shark, sizeof(shark));
     }
-    cout << ret;
 }
 
 int main(void) {
     FastIO();
     Init();
     solve();
+    cout << ret << "\n";
     return 0;
 }
