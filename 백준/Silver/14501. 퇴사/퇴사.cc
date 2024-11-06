9af646aa36d7f0e3db2e477f6bd5d17761da2550
이석환 <104557685+im2sh@users.noreply.github.com>
2024-11-06 22:55:18 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, ret;
+pair<int, int> company[16];
+int DP[21];
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        company[i] = {a, b};
+    }
+}
+
+void solve() {
+    for (int i = 0; i < N; i++) {
+        DP[i + 1] = max(DP[i + 1], DP[i]);
+        if (company[i].first + i > N)
+            continue;
+        DP[i + company[i].first] = max(DP[company[i].first + i], company[i].second + DP[i]);
+    }
+
+    for (int i = 0; i <= N; i++) {
+        ret = max(ret, DP[i]);
+    }
+    cout << ret;
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
