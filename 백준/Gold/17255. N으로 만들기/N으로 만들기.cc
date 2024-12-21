4eba25946ef7f45250d063b3522cd60eafc6eedb
이석환 <104557685+im2sh@users.noreply.github.com>
2024-12-21 23:07:37 +0900

    [Gold IV] Title: N으로 만들기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+string S;
+set<string> ret;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> S;
+}
+
+void go(int left, int right, string str, string path) {
+    if (left == 0 && right == S.length() - 1) {
+        ret.insert(path);
+        return;
+    }
+
+    if (left - 1 >= 0) {
+        go(left - 1, right, S[left - 1] + str, path + S[left - 1] + str);
+    }
+
+    if (right + 1 < S.length()) {
+        go(left, right + 1, str + S[right + 1], path + str + S[right + 1]);
+    }
+}
+
+void solve() {
+    for (int i = 0; i < S.length(); i++) {
+        string path = "";
+        path += S[i];
+        string str = "";
+        str += S[i];
+        go(i, i, str, path);
+    }
+    if (ret.size())
+        cout << ret.size();
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
